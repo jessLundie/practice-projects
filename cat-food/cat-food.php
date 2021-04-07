8832b99bbddbc40748252192daae8b5e9a1f91c1
jessLundie <39742956+jessLundie@users.noreply.github.com>
2021-04-07 14:23:56 -0700

    Update cat-food.php
    Indent revisions & relocation of condition check

--- cat-food/cat-food.php
+++ cat-food/cat-food.php
@@ -2,6 +2,31 @@
 
 // Feeding calculator for cats based on weight and diet type
 
+function how_much_food($cat_weight, $kcalDry, $kcalWet, $dietType, $percentDry, $percentWet) {
+  //constants
+  $tbsConv = 0.0625; // cup / tbs
+
+  $dietMult = array (
+    "Neutered adult cat" => 1.2,
+    "Intact adult cat" => 1.4,
+    "Inactive/obese prone cat" => 1,
+    "Weight loss for cat" => 0.8,
+    "Weight gain for cat" => 1.8,
+    "Kitten 0 to 4 months" => 2.5,
+    "Kitten 4 months to a year" => 2,
+  ); // RER multiplier based on specific factors
+
+  $multiplier = $dietMult[$dietType]; // get multiplier based on diet type
+
+  $kcalFeed = base_caloric_intake($cat_weight) * $multiplier; // calculate total required kcal / day based on weight and diet type
+
+  $qtyDry = round ((($percentDry * $kcalFeed) / $kcalDry) / $tbsConv, 2); // calculate qty dry food(tbs)
+
+  $qtyCan = round (($percentWet * $kcalFeed) / $kcalWet, 2); // calculate qty wet food (cans)
+
+  echo "Feed $qtyDry tbs dry food and $qtyCan cans of wet food daily.";
+}
+
 function base_caloric_intake($cat_weight) {
 	// $cat_weight set in lbs
   // RER / day by weight 1-20lbs ref. Hill’s Pet Nutrition calculator
@@ -27,35 +52,11 @@ function base_caloric_intake($cat_weight) {
     353,
     366,
   );
-	return $kcalBase[$cat_weight-1];
-}
-
-function how_much_food($cat_weight, $kcalDry, $kcalWet, $dietType, $percentDry, $percentWet) {
-  //constants
-  $tbsConv = 0.0625; // cup / tbs
-
-  $dietMult = array (
-    "Neutered adult cat" => 1.2,
-    "Intact adult cat" => 1.4,
-    "Inactive/obese prone cat" => 1,
-    "Weight loss for cat" => 0.8,
-    "Weight gain for cat" => 1.8,
-    "Kitten 0 to 4 months" => 2.5,
-    "Kitten 4 months to a year" => 2,
-  ); // RER multiplier based on specific factors
-
-  $multiplier = $dietMult[$dietType]; // get multiplier based on diet type
-
-  $kcalFeed = base_caloric_intake($cat_weight) * $multiplier; // calculate total required kcal / day based on weight and diet type
-
-  $qtyDry = round ((($percentDry * $kcalFeed) / $kcalDry) / $tbsConv, 2); // calculate qty dry food(tbs)
-  $qtyCan = round (($percentWet * $kcalFeed) / $kcalWet, 2); // calculate qty wet food (cans)
 
   if (($cat_weight >= 1) && ($cat_weight <= 20)) {
-
-    echo "Feed $qtyDry tbs dry food and $qtyCan cans of wet food daily.";
-
+    return $kcalBase[$cat_weight-1];
+    call_user_func('how_much_food');
   } else {
-    echo "Please enter a weight between 1 and 20";
+    echo "Please enter a weight between 1 and 20.";
   }
 }
