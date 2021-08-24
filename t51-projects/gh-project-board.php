be130a1bbcdfbd6473cc2e0d1c0804f0da14fa65
jessLundie <39742956+jessLundie@users.noreply.github.com>
2021-08-24 16:34:43 -0700

    cURL function revision and date sorting addition
    Revised base cURL function and added functionality to alert for the number of past-due issues.

--- t51-projects/gh-project-board.php
+++ t51-projects/gh-project-board.php
@@ -1,32 +1,31 @@
 <?php
 
 
-function curl_fetch() {
+$url = 'https://api.github.com/users/jessLundie'; // Set source URL
 
-	$url = 'https://api.github.com/users/jessLundie'; // Set source URL
+$media_header = array(
+	"Accept: application/vnd.github.inertia-preview+json",
+); // Custom media header required for projects in beta
 
-	$column_id = "15445284"; // Column id, found in the column link here: https://d.pr/i/W7RQJB
+$user_agent = "jessLundie"; // Set user agent
 
-	$project_url = "https://api.github.com/projects/columns/" . $column_id . "/cards"; // Set project URL
+$token = ""; // Basic Auth Token
 
-	$token = "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"; // Basic Auth Token
+$column_id = "15445284"; // Column id, found in the column link here: https://d.pr/i/W7RQJB
 
-	$user_agent = "jessLundie"; // Set user agent
+$project_url = "https://api.github.com/projects/columns/" . $column_id . "/cards"; // Set API URL for project
 
-	// Custom media header required for projects in beta
-	$headers = array(
-		"Accept: application/vnd.github.inertia-preview+json",
-	);
+function curl_fetch( $url, $media_header, $user_agent, $token, $api_url ) {
 
 	// Create a new cURL resource
 	$process = curl_init($url);
 
 	// Set options
-	curl_setopt( $process, CURLOPT_HTTPHEADER, $headers );
+	curl_setopt( $process, CURLOPT_HTTPHEADER, $media_header );
 	curl_setopt( $process, CURLOPT_USERAGENT, $user_agent );
 	curl_setopt( $process, CURLOPT_USERPWD, "$user_agent:$token" );
 	curl_setopt( $process, CURLOPT_RETURNTRANSFER, TRUE);
-	curl_setopt( $process, CURLOPT_URL, $project_url );
+	curl_setopt( $process, CURLOPT_URL, $api_url );
 
 	// Get project details
 	$result = curl_exec( $process );
@@ -34,12 +33,38 @@ function curl_fetch() {
 	// Close cURL
 	curl_close( $process );
 
+	$result = json_decode( $result, true ); // Convert response to array
 	return $result;
+
 }
 
-$response = curl_fetch();
-$response = json_decode( $response );
+$cards = curl_fetch( $url, $media_header, $user_agent, $token, $project_url ); // List of cards
 
-$cards = count( $response );
+// Create a list of all labels
 
-echo "There are $cards issues in the Needs Triaged queue.";
+$label_list = [];
+
+foreach ( $cards as $card ) {
+
+	$label_url = $card['content_url'] . "/labels"; // Set API URL for labels
+
+	$label = curl_fetch( $url, $media_header, $user_agent, $token, $label_url  ); // API call to fetch labels
+
+	$label_list = array_merge( $label_list, $label ); // Add each label to list of labels
+}
+
+// Count issues where due date is before the current date
+
+$issues_overdue = 0;
+
+foreach ( $label_list as $label ) {
+
+	$issue_date =  str_replace( "[Due Date] ", '', $label['name'] ) . " " . date( 'Y' );
+
+	if ( strtotime( $issue_date ) < time() ) {
+
+		$issues_overdue++;
+	}
+}
+
+echo "There are $issues_overdue issues in the Needs Triaged queue that are past due or due today. Could someone please take a look?";
